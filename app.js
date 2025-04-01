f89c7bae17fc7a9f0b3ce824419db00773b35478
Vidya <vidyayerra7@gmail.com>
2025-04-01 20:42:57 +0530

    changed port

--- app.js
+++ app.js
@@ -43,8 +43,8 @@ if (!process.env.MONGO_URI) {
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB Atlas');
-    app.listen(process.env.PORT || 5000, () => {
-      console.log(`Server running on port ${process.env.PORT || 5000}`);
+    app.listen(process.env.PORT || 10000, () => {
+      console.log(`Server running on port ${process.env.PORT || 10000}`);
     });
   })
   .catch(err => {
