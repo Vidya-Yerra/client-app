0e9cdb17d00f062fd9c494fb647bece21cb9f4d5
Vidya <vidyayerra7@gmail.com>
2025-04-02 02:02:02 +0530

    corrected url

--- clientapp-frontend/src/app/home/clients/page.js
+++ clientapp-frontend/src/app/home/clients/page.js
@@ -127,7 +127,7 @@ const handleSavePage = async (updatedClients) => {
   
         const payload = { payments: formattedPayments };
   
-        const res = await fetch(`https://client-app-6.onrender.com//payments/${client.clientId}/payments`, {
+        const res = await fetch(`https://client-app-6.onrender.com/payments/${client.clientId}/payments`, {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
